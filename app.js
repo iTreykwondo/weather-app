48e668766623bcb71df6358878ed5af2b6f3b3fe
Trajan Miller <trajanmiller324@gmail.com>
2022-04-05 21:13:03 -0400

    Populate weather card with weather data

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
-const img = document.querySelector("#icon");
 const button = document.querySelector("button");
 const searchBar = document.querySelector("input");
 const errorDiv = document.querySelector(".error");
-const card = document.querySelector(".card");
-let condition = document.querySelector("#condition");
-let cityH2 = document.querySelector("#city-name");
 
 async function fetchWeatherData(city) {
   city = searchBar.value;
 
-  const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=461bdbb7ad11819525963162ffe0d2a9&units=imperial`,
-    { mode: "cors" }
-  );
-  const weatherData = await response.json();
-  console.log(weatherData);
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=461bdbb7ad11819525963162ffe0d2a9&units=imperial`,
+      { mode: "cors" }
+    );
 
-  return weatherData;
+    const weatherData = await response.json();
+    updateUI(weatherData);
+
+    return weatherData;
+  } catch (error) {
+    errorDiv.textContent = error.message;
+  }
+}
+
+function updateUI(weatherData) {
+  const img = document.querySelector("#icon");
+  let condition = document.querySelector("#condition");
+  let cityH2 = document.querySelector("#city-name");
+  let wind = document.querySelector("#wind-value");
+  let temperature = document.querySelector("#temp-value");
+  let feelsLike = document.querySelector("#feels-like-temp");
+
+  let location = `${weatherData.name}, ${weatherData.sys.country}`;
+
+  img.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+  condition.textContent = weatherData.weather[0].main;
+  cityH2.textContent = location;
+  wind.textContent = `${weatherData.wind.speed} mph`;
+  temperature.textContent = `${weatherData.main.temp}°F`;
+  feelsLike.textContent = `${weatherData.main.feels_like}°F`;
+
+  errorDiv.textContent = "";
 }
 
 button.addEventListener("click", fetchWeatherData);
